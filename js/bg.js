b4984add55413397e28e1919b07db8860f4c5b2d
clouderMark <charnysheuski@yandex.by>
2022-09-07 10:31:42 +0300

    fix: img addresses

--- js/bg.js
+++ js/bg.js
@@ -5,15 +5,15 @@ for (let radio of radios) {
     radio.addEventListener('click', function () {
         val = radio.value;
         if (val == 1) {
-            document.body.style.backgroundImage = "url('../img/iceCream1.png'), url('../img/backLight1.png')"; 
+            document.body.style.backgroundImage = "url('img/iceCream1.png'), url('img/backLight1.png')"; 
             document.body.style.backgroundColor = "#849d8f";
         }
         if (val == 2) {
-            document.body.style.backgroundImage = "url('../img/iceCream2.png'), url('../img/backLight2.png')"; 
+            document.body.style.backgroundImage = "url('img/iceCream2.png'), url('img/backLight2.png')"; 
             document.body.style.backgroundColor = "#8996a6";
         }
         if (val == 3) {
-            document.body.style.backgroundImage = "url('../img/iceCream3.png'), url('../img/backLight3.png')"; 
+            document.body.style.backgroundImage = "url('img/iceCream3.png'), url('img/backLight3.png')"; 
             document.body.style.backgroundColor = "#9d8b84";
         }
     })
